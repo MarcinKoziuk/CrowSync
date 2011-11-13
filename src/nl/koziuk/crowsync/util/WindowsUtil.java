b0bc4fc95c2bfd8f6d4ebcc02c4fdf429a9d2663
Marcin Koziuk <marcin.koziuk@gmail.com>
2011-11-13 21:26:15 +0100

    Used proper exec() method for performing shell operations. This fixes a bug that prevented some files from being copied.

--- src/nl/koziuk/crowsync/util/WindowsUtil.java
+++ src/nl/koziuk/crowsync/util/WindowsUtil.java
@@ -15,57 +15,62 @@ import java.util.Set;
  */
 public class WindowsUtil {
 
-    // TODO: make linux version with the ps command
+	// TODO: make linux version with the ps command
 
-    /**
-     * Returns list of running processes. Only works on windows.
-     * 
-     * @return List of currently running processes.
-     */
-    public static Set<String> listRunningProcesses() {
+	/**
+	 * Returns list of running processes. Only works on windows.
+	 * 
+	 * @return List of currently running processes.
+	 */
+	public static Set<String> listRunningProcesses() {
 
-        Set<String> processes = new HashSet<String>();
+		Set<String> processes = new HashSet<String>();
 
-        try {
-            String line;
-            Process p = Runtime.getRuntime().exec("tasklist.exe /fo csv /nh");
-            BufferedReader input = new BufferedReader
-                    (new InputStreamReader(p.getInputStream()));
-            while ((line = input.readLine()) != null) {
-                if (!line.trim().equals("")) {
-                    // keep only the process name
-                    line = line.substring(1);
-                    processes.add(line.substring(0, line.indexOf("\"")));
-                }
+		try {
+			String line;
+			Process p = Runtime.getRuntime().exec("tasklist.exe /fo csv /nh");
+			BufferedReader input = new BufferedReader(new InputStreamReader(
+					p.getInputStream()));
+			while ((line = input.readLine()) != null) {
+				if (!line.trim().equals("")) {
+					// keep only the process name
+					line = line.substring(1);
+					processes.add(line.substring(0, line.indexOf("\"")));
+				}
 
-            }
-            input.close();
-        } catch (Exception err) {
-            err.printStackTrace();
-        }
-        return processes;
-    }
+			}
+			input.close();
+		} catch (Exception err) {
+			err.printStackTrace();
+		}
+		return processes;
+	}
 
-    /**
-     * Because Java 6 doesn't support file copying.
-     * 
-     * @param sourceFile The source file.
-     * @param destDir The destination file.
-     * @return Whether the copying succeeded or not.
-     */
-    public static boolean copyFileToDir(File sourceFile, File destDir) {
-        if (!sourceFile.exists() | !sourceFile.isFile() | !destDir.isDirectory()) {
-            throw new IllegalArgumentException();
-        }
+	/**
+	 * Because Java 6 doesn't support file copying.
+	 * 
+	 * @param sourceFile
+	 *            The source file.
+	 * @param destDir
+	 *            The destination file.
+	 * @return Whether the copying succeeded or not.
+	 */
+	public static boolean copyFileToDir(File sourceFile, File destDir) {
+		if (!sourceFile.exists() | !sourceFile.isFile()
+				| !destDir.isDirectory()) {
+			throw new IllegalArgumentException();
+		}
 
-        try {
-            Process process = Runtime.getRuntime().exec(
-                    "cmd /c copy \"" + sourceFile.getAbsolutePath() + "\" \"" + destDir.getAbsolutePath() + "\\\"");
-            process.waitFor();
-        } catch (IOException | InterruptedException e) {
-            return false;
-        }
+		try {
+			Process process = Runtime.getRuntime().exec(
+					new String[] { "cmd", "/c", "copy",
+							sourceFile.getAbsolutePath(),
+							destDir.getAbsolutePath() });
+			process.waitFor();
+		} catch (IOException | InterruptedException e) {
+			return false;
+		}
 
-        return true;
-    }
+		return true;
+	}
 }
